8bf615f97106f77f7dabaa2855a2edda264e3719
Brian Coords <bacoords@gmail.com>
2024-03-15 15:40:17 -0700

    adds ollie child theme

---
+++ wp-content/themes/olliechild/patterns/index.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Title: index
+ * Slug: olliechild/index
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"0","top":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio","fontSize":"xx-large"} -->
+<h1 class="wp-block-heading wp-embed-aspect-16-9 wp-has-aspect-ratio has-xx-large-font-size" style="font-style:normal;font-weight:400">Exploring <strong>Modern </strong><br><strong>WordPress Development</strong></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"verticalAlignment":"bottom","align":"wide","className":"reverse-on-mobile"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-bottom reverse-on-mobile"><!-- wp:column {"verticalAlignment":"bottom","width":"33%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:33%"><!-- wp:image {"id":8636,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/IMG_7907-3-Background-Removed-1024x768.png" alt="" class="wp-image-8636"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"fontSize":"x-large"} -->
+<p class="has-x-large-font-size"><strong><mark style="background-color:#233240;color:#ffffff" class="has-inline-color">Hi, I'm Brian!</mark></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>I'm helping WordPress developers and agencies make the transition into the new era of the WordPress Block Editor. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Subscribe below and find me on <a href="https://www.youtube.com/c/BrianCoords"><mark style="background-color:#cf2e2e;color:#ffffff" class="has-inline-color"><strong>YouTube</strong></mark></a> for WordPress tutorials around development topics like custom blocks and Gutenberg.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}},"backgroundColor":"tertiary","textColor":"foreground","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-foreground-color has-tertiary-background-color has-text-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66%"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading -->
+<h2 class="wp-block-heading">Grow Your WordPress Dev Skills</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Every week I'm sharing the best links and my latest articles and videos about all things WordPress, with a big focus on mastering the block and site editor ("Gutenberg").</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:convertkit/form {"form":"3984970"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--large)"><!-- wp:heading -->
+<h2 class="wp-block-heading">✏️ All the Latest Everything</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"primary","style":{"spacing":{"padding":{"left":"var:preset|spacing|20","right":"var:preset|spacing|20","top":"0","bottom":"0"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color wp-element-button" href="https://www.briancoords.com/blog/" style="padding-top:0;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)">Blog</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"textColor":"primary","style":{"spacing":{"padding":{"left":"var:preset|spacing|20","right":"var:preset|spacing|20","top":"0","bottom":"0"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color wp-element-button" href="https://www.briancoords.com/videos/" style="padding-top:0;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)">Videos</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"textColor":"primary","style":{"spacing":{"padding":{"left":"var:preset|spacing|20","right":"var:preset|spacing|20","top":"0","bottom":"0"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color wp-element-button" href="https://www.briancoords.com/tutorials/" style="padding-top:0;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)">Tutorials</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"textColor":"primary","style":{"spacing":{"padding":{"left":"var:preset|spacing|20","right":"var:preset|spacing|20","top":"0","bottom":"0"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color wp-element-button" href="https://www.briancoords.com/newsletter/" style="padding-top:0;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)">Newsletters</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:query {"queryId":1,"query":{"perPage":10,"pages":"0","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":true},"metadata":{"categories":["olliechild/posts"]},"align":"full"} -->
+<div class="wp-block-query alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-bottom:0"><!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"default","columnCount":2},"fontSize":"large"} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"5px"}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="border-radius:5px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"category","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"700","textDecoration":"underline"},"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"primary","textColor":"base","fontSize":"x-small"} /-->
+
+<!-- wp:post-date {"textColor":"secondary","fontSize":"x-small"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"typography":{"textDecoration":"none"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"medium"} /-->
+
+<!-- wp:post-excerpt {"moreText":"","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"className":"is-style-excerpt-truncate-3","fontSize":"small"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--x-large)"><!-- wp:query-pagination {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:query-pagination-previous {"className":"is-style-wp-block-button__link"} /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next {"backgroundColor":"primary","className":"is-style-wp-block-button__link"} /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:query --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
---
+++ wp-content/themes/olliechild/readme.txt
@@ -0,0 +1,54 @@
+=== Ollie Child ===
+Contributors: bacoords
+Requires at least: 6.0
+Tested up to: 6.5-RC2-57821
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Custom child theme of ollie
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+Ollie Child is a child theme of Ollie (http://olliewp.com), (C) Mike McAlister, [GNU General Public License v3.0 (or later)](https://www.gnu.org/licenses/gpl-3.0.html)
+
+Ollie Theme, (C) 2023 Mike McAlister
+Ollie is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+All media licensed under Creative Commons Zero (CC0) https://creativecommons.org/publicdomain/zero/1.0/
+
+skateboarding.webp  - https://stocksnap.io/photo/skateboarder-sidewalk-NH8J97NEVN
+computer-hands.webp - https://stocksnap.io/photo/computer-laptop-FBXB2DA8O7
+avatar-1.webp       - https://stocksnap.io/photo/people-man-A3WDGDTBI6
+avatar-2.webp       - https://stocksnap.io/photo/urban-fashion-TQAKNY0XO2
+avatar-3.webp       - https://stocksnap.io/photo/woman-glasses-7RKWHUXLMQ
+avatar-4.webp       - https://stocksnap.io/photo/smiling-woman-KS92MVGSXY
+avatar-5.webp       - https://stocksnap.io/photo/male-professional-6QXAIH13O6
+avatar-7.webp       - https://stocksnap.io/photo/woman-business-LERRJPTMHP
+rocket.webp         - https://stocksnap.io/photo/rocketship-spaceship-IETQP9ZADV
+
+logo-1.webp, logo-2.webp, logo-3.webp, logo-4.webp, logo-5.webp - created by Mike McAlister and available via CC0.
+All images in /inc/settings/build/images/ created by Mike McAlister and available via CC0. 
+
+Other assets: 
+
+- The Inter font is available via the SIL Open Font License 1.1: https://github.com/rsms/inter/blob/master/LICENSE.txt
+- Icons available from Iconnoir via the MIT License: https://github.com/iconoir-icons/iconoir/blob/main/LICENSE
+
