f9929ba0f93fce0c15a3cc7b47349d0d2759d077
Brian Coords <bacoords@gmail.com>
2024-03-15 15:46:03 -0700

    updates header to include avatar

---
+++ wp-content/themes/olliechild/patterns/header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: header
+ * Slug: olliechild/header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"metadata":{"categories":["header"]},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|main"}}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":8636,"width":"40px","height":"40px","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-large is-resized is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/IMG_7907-3-Background-Removed-1024x768.png" alt="" class="wp-image-8636" style="object-fit:cover;width:40px;height:40px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"fontSize":"base"} /-->
+
+<!-- wp:paragraph {"fontSize":"x-small"} -->
+<p class="has-x-small-font-size">WordPress Developer & Educator</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":6389,"textColor":"main","icon":"menu","overlayBackgroundColor":"base","overlayTextColor":"main","layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"20px"}},"fontSize":"small"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
