a6d6c061d86f84977107e45427f21d3bebf91299
Brian Coords <bacoords@gmail.com>
2025-09-16 06:26:31 -0700

    removes newsletter cpt code

--- wp-content/plugins/bac-functionality/bac-functionality.php
+++ wp-content/plugins/bac-functionality/bac-functionality.php
@@ -60,20 +60,6 @@ function bc_add_link_tag( $title, $id ) {
 add_filter( 'the_title', __NAMESPACE__ . '\bc_add_link_tag', 10, 2 );
 
 /**
- * Add a custom post type to the front page query.
- *
- * @param WP_Query $query The main query.
- * @return WP_Query The main query.
- */
-function bc_add_cpt_to_front_page_query( $query ) {
-	if ( $query->is_main_query() && ! is_admin() && ( is_home() || is_feed() ) ) {
-		$query->set( 'post_type', array( 'post', 'newsletter' ) );
-	}
-	return $query;
-}
-// add_action( 'pre_get_posts', __NAMESPACE__ . '\bc_add_cpt_to_front_page_query' );
-
-/**
  * Register a custom post type called "7 Tools".
  *
  * @see get_post_type_labels() for label keys.
@@ -126,62 +112,6 @@ function bcdotcom_seven_tools_cpt_init() {
 	register_post_type( 'seven_tools', $args );
 }
 add_action( 'init', __NAMESPACE__ . '\bcdotcom_seven_tools_cpt_init' );
-
-/**
- * Register a custom post type called "Newsletter".
- *
- * @see get_post_type_labels() for label keys.
- */
-function bcdotcom_newsletter_cpt_init() {
-	$labels = array(
-		'name'                  => _x( 'Newsletters', 'Post type general name', 'textdomain' ),
-		'singular_name'         => _x( 'Newsletter', 'Post type singular name', 'textdomain' ),
-		'menu_name'             => _x( 'Newsletters', 'Admin Menu text', 'textdomain' ),
-		'name_admin_bar'        => _x( 'Newsletter', 'Add New on Toolbar', 'textdomain' ),
-		'add_new'               => __( 'Add New', 'textdomain' ),
-		'add_new_item'          => __( 'Add New Newsletter', 'textdomain' ),
-		'new_item'              => __( 'New Newsletter', 'textdomain' ),
-		'edit_item'             => __( 'Edit Newsletter', 'textdomain' ),
-		'view_item'             => __( 'View Newsletter', 'textdomain' ),
-		'all_items'             => __( 'All Newsletters', 'textdomain' ),
-		'search_items'          => __( 'Search Newsletters', 'textdomain' ),
-		'parent_item_colon'     => __( 'Parent Newsletters:', 'textdomain' ),
-		'not_found'             => __( 'No Newsletters found.', 'textdomain' ),
-		'not_found_in_trash'    => __( 'No Newsletters found in Trash.', 'textdomain' ),
-		'featured_image'        => _x( 'Newsletter Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-		'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-		'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-		'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-		'archives'              => _x( 'Newsletter archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-		'insert_into_item'      => _x( 'Insert into Newsletter', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-		'uploaded_to_this_item' => _x( 'Uploaded to this Newsletter', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-		'filter_items_list'     => _x( 'Filter Newsletters list', 'Screen reader text for the filter Newsletters heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-		'items_list_navigation' => _x( 'Newsletters list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-		'items_list'            => _x( 'Newsletters list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
-	);
-
-	$args = array(
-		'labels'             => $labels,
-		'public'             => true,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-		'query_var'          => true,
-		'rewrite'            => 'newsletter',
-		'show_in_rest'       => 'true',
-		'capability_type'    => 'post',
-		'has_archive'        => 'newsletter',
-		'hierarchical'       => false,
-		'menu_icon'          => 'dashicons-email-alt2',
-		'menu_position'      => null,
-		'supports'           => array( 'title', 'editor', 'revisions', 'author', 'excerpt', 'comments' ),
-		'taxonomies'         => array( 'category' ),
-	);
-
-	register_post_type( 'newsletter', $args );
-}
-// add_action( 'init', __NAMESPACE__ . '\bcdotcom_newsletter_cpt_init' );
-
 
 
 /**
