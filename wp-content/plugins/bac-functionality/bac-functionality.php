725833486d27395d948373d04c9f48beccf9607b
Brian Coords <bacoords@gmail.com>
2025-09-15 20:24:10 -0700

    adds 7 tools cpt

--- wp-content/plugins/bac-functionality/bac-functionality.php
+++ wp-content/plugins/bac-functionality/bac-functionality.php
@@ -74,6 +74,60 @@ function bc_add_cpt_to_front_page_query( $query ) {
 // add_action( 'pre_get_posts', __NAMESPACE__ . '\bc_add_cpt_to_front_page_query' );
 
 /**
+ * Register a custom post type called "7 Tools".
+ *
+ * @see get_post_type_labels() for label keys.
+ */
+function bcdotcom_seven_tools_cpt_init() {
+	$labels = array(
+		'name'                  => _x( '7 Tool Pages', 'Post type general name', 'textdomain' ),
+		'singular_name'         => _x( '7 Tool Page', 'Post type singular name', 'textdomain' ),
+		'menu_name'             => _x( '7 Tool Pages', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar'        => _x( '7 Tool Page', 'Add New on Toolbar', 'textdomain' ),
+		'add_new'               => __( 'Add New', 'textdomain' ),
+		'add_new_item'          => __( 'Add New 7 Tool Page', 'textdomain' ),
+		'new_item'              => __( 'New 7 Tool Page', 'textdomain' ),
+		'edit_item'             => __( 'Edit 7 Tool Page', 'textdomain' ),
+		'view_item'             => __( 'View 7 Tool Page', 'textdomain' ),
+		'all_items'             => __( 'All 7 Tool Pages', 'textdomain' ),
+		'search_items'          => __( 'Search 7 Tool Pages', 'textdomain' ),
+		'parent_item_colon'     => __( 'Parent 7 Tool Pages:', 'textdomain' ),
+		'not_found'             => __( 'No 7 Tool Pages found.', 'textdomain' ),
+		'not_found_in_trash'    => __( 'No 7 Tool Pages found in Trash.', 'textdomain' ),
+		'featured_image'        => _x( '7 Tool Page Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'archives'              => _x( '7 Tool Page archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+		'insert_into_item'      => _x( 'Insert into 7 Tool Page', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+		'uploaded_to_this_item' => _x( 'Uploaded to this 7 Tool Page', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+		'filter_items_list'     => _x( 'Filter 7 Tool Pages list', 'Screen reader text for the filter 7 Tool Pages heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+		'items_list_navigation' => _x( '7 Tool Pages list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+		'items_list'            => _x( '7 Tool Pages list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => '7-tools',
+		'show_in_rest'       => 'true',
+		'capability_type'    => 'post',
+		'has_archive'        => false,
+		'hierarchical'       => false,
+		'menu_icon'          => 'dashicons-email-alt2',
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor', 'revisions', 'author', 'excerpt', 'comments' ),
+	);
+
+	register_post_type( 'seven_tools', $args );
+}
+add_action( 'init', __NAMESPACE__ . '\bcdotcom_seven_tools_cpt_init' );
+
+/**
  * Register a custom post type called "Newsletter".
  *
  * @see get_post_type_labels() for label keys.
